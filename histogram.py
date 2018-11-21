f8a3acad022c4e63ae1f23aaefd1c8216497da5f
Nicholas Williams <nwilliams770@gmail.com>
2018-11-20 16:15:10 -0800

    add markov model

---
+++ histogram.py
@@ -0,0 +1,44 @@
+## TRAINING:
+# Instantiate some sort of dict or dict-like data structure
+# Iterate over corpus, keeping count of unique words, total words, and adding words as keys and frequencies as values
+from random import randint, choices
+
+class FrequencyGram(dict):
+    # Probably better practice to inherit from MutableMapping but then we'd have to rewrite all 5 of those methods
+    # Could we also just have a property like self.dict and access it that way?
+    def __init__(self, iterable=None):
+        super(FrequencyGram, self)
+        self.tokens = 0 # total for specific window, not entire model
+        self.types = 0 # total for specific window, not entire model
+        if iterable:
+            self.update(iterable)
+    
+    def update(self, iterable):
+        for item in iterable:
+            if item in self:
+                self[item] += 1
+                self.tokens += 1
+            else:
+                self[item] = 1
+                self.tokens += 1
+                self.types += 1
+    
+    def count(self, item):
+        if item in self:
+            return self[item]
+        return 0
+
+    def return_rand_word(self):
+        return self.keys()[randint(0, self.types - 1)]
+
+    def return_weighted_rand_word(self):    
+        weights = self.create_probability_distribution()
+        return choices(list(self.keys()), weights)
+
+    def create_probability_distribution(self):
+        occurences = self.values()
+        distribution = []
+        total = sum(occurences)
+        for occurence in occurences:
+                distribution.append(occurence / total)
+        return distribution
