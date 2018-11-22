a32ca6677e799767b5a46bec8392ddf525324d79
Nicholas Williams <nwilliams770@gmail.com>
2018-11-21 17:46:48 -0800

    refactor make_markov and add method for n length sentence formatting

--- markov.py
+++ markov.py
@@ -1,12 +1,19 @@
 from histogram import FrequencyGram
 import re
+import pprint
+
 
 # TO-DO:
+## FIX GENERATING SENTENCES:
+    # need to capitalize first word, 
+    # also, how to deal without spacing punctuation
+## push up
+## write readme
+
 ## - generate_random_start
 ##- generate_n_length_sentence
 ## data parser:
-    # we probably want to remove all quotes, single and double
-    # - reads line by line, putting all 
+## 
 
 ALPHABETS= "([A-Za-z])"
 PREFIXES = "(Mr|St|Mrs|Ms|Dr)[.]"
@@ -14,24 +21,53 @@ SUFFIXES = "(Inc|Ltd|Jr|Sr|Co)"
 STARTERS = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
 ACRONYMS = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
 WEBSITES = "[.](com|net|org|io|gov)"
+PUNCS = ",'.?!:;"
 
-def make_markov_model(data):
+def make_markov_model(corpus):
     markov_model = dict()
-
-    for sentence in data:
-        if "START" in markov_model and "END" in markov_model:
+    corpus_size = 0
+    # print(corpus)
+    for sentence in corpus:
+        # current_sentence = [word.lower() for word in sentence] # lowercase all words
+        # print(current_sentence)
+        if "START" in markov_model:
             markov_model["START"].update(sentence[0])
-            markov_model["END"].update(sentence[-1])
         else:
             markov_model["START"] = FrequencyGram(sentence[0])
-            markov_model["END"] = FrequencyGram(sentence[-1])
-        for i in range(0, len(sentence)-1):
-            if sentence[i] in markov_model:
+
+        if sentence[-1] in markov_model: #have to make it manually point to END
+                markov_model[sentence[-1]].update("END")
+        else:
+            markov_model[sentence[-1]] = FrequencyGram("END")
+        corpus_size += len(sentence)
+        for i in range(0, len(sentence)-1): ## minus two because all sentence terminators point to END
                 # Note, we might need to update this or the .update func
                 # depending how we want it to parse the data
+            if sentence[i] in markov_model:
                 markov_model[sentence[i]].update(sentence[i+1])
             else:
                 markov_model[sentence[i]] = FrequencyGram(sentence[i+1])
+
+    markov_model["END"] = FrequencyGram("START") ## Since we're adding in START/END has we iterate instead of having it in the raw data,
+                                                 ## we have to manually add a FrequencyGram at key END to ensure our sentences flow
+    print(f"Corpus: {corpus_size} words (punctuation included)")
+    return markov_model
+
+def load_data(file_path):
+    """ Open and format data from file_path(s), returns a trained markov model """
+    if isinstance(file_path, list):
+        for path in file_path:
+            corpus = []
+            raw_data = get_data(path)
+            corpus += split_into_sentences(raw_data)
+            
+
+        markov_model = make_markov_model(corpus)
+    else:
+        raw_data = get_data(file_path)
+        corpus = split_into_sentences(raw_data)
+        markov_model = make_markov_model(corpus)
+        # print(raw_data)
     return markov_model
 
 
@@ -46,14 +82,29 @@ def generate_n_length_sentence(length, markov_model):
         current_frequencygram = markov_model[current_word]
         next_word = current_frequencygram.return_weighted_rand_word()
         current_word = next_word
+        if current_word == "START" or current_word == "END":
+            continue
         sentence.append(current_word)
-    sentence[0].captalize()
-    return " ".join(sentence).replace("END", "")
+    print(sentence)
+    return format_text(sentence)
+
+def format_text(sentence_list):
+    formatted = ""
+    for i in range(0, len(sentence_list)):
+        if sentence_list[i] not in PUNCS:
+            formatted += sentence_list[i] + " "
+        elif sentence_list[i] in PUNCS and i == 0:
+            formatted += sentence_list[i]
+        else:
+            formatted = formatted[:-1] + sentence_list[i] + " "
+    return formatted
 
 def generate_n_sentences(length, markov_model):
     current_word = generate_random_start(markov_model)
-    sentences = [[] for i in range(10)]
-    sentences[0].append(current_word)
+
+    sentences = []
+    sentences.append(current_word)
+
     for i in range(0, length):
         while True:
             current_frequencygram = markov_model[current_word]
@@ -61,8 +112,30 @@ def generate_n_sentences(length, markov_model):
             if next_word == "END":
                 break
             current_word = next_word
-            sentences[i].append(current_word)
-        current_word = sentences[i][-1]
+            if current_word not in PUNCS:
+                sentences[i] += " " + current_word
+            else:
+                sentences[i] += current_word
+            sentences[i].capitalize()
+    return " ".join(sentences)
+
+    # sentences = [[] for i in range(length)]
+    # sentences[0].append(current_word)
+    # for i in range(0, length):
+    #     while True:
+    #         current_frequencygram = markov_model[current_word]
+    #         next_word = current_frequencygram.return_weighted_rand_word()
+    #         if next_word == "END":
+    #             break
+    #         current_word = next_word
+    #         sentences[i].append(current_word)
+    #     if i > 0:
+    #         current_word = sentences[i - 1][-1]
+    # result = ""
+    # for sentence in sentences:
+    #     result += " ".join(sentence)
+    #     result[0].upper()
+    # return result
 
 def get_data(path):
     with open(path) as file:
@@ -99,8 +172,22 @@ def split_into_sentences(text):
     sentences = sentences[:-1]
     sentences = [s.strip() for s in sentences]
     ## Added line to split sentence lists into lists of words/punctuation
-    sentences = [ re.findall(r"[\w]+|[^\s\w]", sentence) for sentence in test2 ]
+    # sentences = [ re.findall(r"[\w]+|[^\s\w]", sentence) for sentence in text ]
+    for i in range(0, len(sentences)):
+        sentences[i] = re.findall(r"[\w']+|[.,!?;]", sentences[i])
+    # pprint.pprint(sentences)
     return sentences
 
-test = get_data("./fellowship_of_the_ring.txt")
+training_set_1 = ["./fellowship_of_the_ring.txt", "./two_towers.txt", "./return_of_the_king.txt"]
+training_set_2 = "./fellowship_of_the_ring.txt"
+# model_1 = load_data(training_set_1)
+model_2 = load_data(training_set_2)
+
+# test = generate_n_length_sentence(30, model_1)
+# test = generate_n_sentences(3, model_1)
+test2 = generate_n_length_sentence(30, model_2)
+print(test2)
+# print(test2)
+# pprint.pprint(model_1)
+
 
